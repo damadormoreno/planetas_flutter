68e19d46591850ce14b73591ecb313051de80b14
David Amador <amador.moreno.david@gmail.com>
2018-04-12 23:51:24 +0200

    appbar

---
+++ lib/ui/home/PlanetRow.dart
@@ -0,0 +1,19 @@
+import 'package:flutter/material.dart';
+
+class PlanetRow extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return new Container(
+      margin: const EdgeInsets.symmetric(
+        vertical: 16.0,
+        horizontal: 24.0,
+      ),
+      child: new Stack(
+        children: <Widget>[
+          planetCard,
+          planetThumbnail,
+        ],
+      ),
+    );
+  }
+}
